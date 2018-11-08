b657360675c0092525acb38fcbf7fcea8fe781f9
Agilatech <scott@agilatech.com>
2018-11-08 15:29:58 -0700

    change to new device instantiation

--- bme280.js
+++ bme280.js
@@ -3,18 +3,9 @@ const LynxariDevice = require(process.lynxari.device);
 const device = require('@agilatech/bme280');
 
 module.exports = class Bme280 extends LynxariDevice {
-    
     constructor(config) {
-        
-        // The bus/file must be defined. If not supplied in config, then default to i2c-2
-        const bus  = config['bus'] || "/dev/i2c-2";
-        const altitude = (typeof config['altitude'] != 'undefined') ? config['altitude'] : 5;
-        
-        const hardware = new device.Bme280(bus, altitude);
-        
+        const hardware = new device(config);
         super(hardware, config);
-        
     }
-    
 }
 
